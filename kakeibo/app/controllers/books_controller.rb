d2755df9b3069c9e12c51488e191a63bb974e27e
ayumyonn <au_ddddd@icloud.com>
2026-03-12 14:07:20 +0900

    新規登録画面の作成

--- kakeibo/app/controllers/books_controller.rb
+++ kakeibo/app/controllers/books_controller.rb
@@ -9,6 +9,19 @@ class BooksController < ApplicationController
 
   def new
     @book = Book.new
-    #@book.year = Time.zone.today.year
+    @book.year = Time.zone.today.year
+  end
+
+  def create
+    endbook_params = params.require(:book).permit(:year,:month,:inout,:category,:amount)
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:inout,:category,:year,:month,:amount)
   end
 end
