3136567c20c33dd230ffe5796b0dfa2ed88cac0c
ayumyonn <au_ddddd@icloud.com>
2026-03-12 13:45:07 +0900

    新規登録画面の作成

--- kakeibo/app/controllers/books_controller.rb
+++ kakeibo/app/controllers/books_controller.rb
@@ -6,4 +6,9 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+
+  def new
+    @book = Book.new
+    #@book.year = Time.zone.today.year
+  end
 end
