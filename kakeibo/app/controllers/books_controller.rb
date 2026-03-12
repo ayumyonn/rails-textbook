16e23865a6e7344d3c41c997251c8ef071feb01e
ayumyonn <au_ddddd@icloud.com>
2026-03-12 14:30:38 +0900

    new画面のバリデーションと新規登録ボタンの追加

--- kakeibo/app/controllers/books_controller.rb
+++ kakeibo/app/controllers/books_controller.rb
@@ -15,8 +15,14 @@ class BooksController < ApplicationController
   def create
     endbook_params = params.require(:book).permit(:year,:month,:inout,:category,:amount)
     @book = Book.new(book_params)
-    @book.save
-    redirect_to books_path
+
+    if @book.save
+      redirect_to books_path
+    else
+      render :new
+    end
+    #@book.save
+    #redirect_to books_path
   end
 
   private
